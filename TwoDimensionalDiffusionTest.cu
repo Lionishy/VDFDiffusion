17ca5d97a9ff570d76e2293daad8e9682e668c19
Lionishy <lionishy@gmail.com>
2019-10-21 14:24:23 +0300

    Simple test completed

--- TwoDimensionalDiffusionTest.cu
+++ TwoDimensionalDiffusionTest.cu
@@ -1,4 +1,5 @@
 #include "ForwardStepKernel.cuh"
+#include "ThomsonSolverKernel.cuh"
 
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
@@ -7,6 +8,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <chrono>
 
 template <typename T>
 void initial_value(std::vector<T> &matrix, size_t x_size, size_t y_size) {
@@ -49,7 +51,7 @@ int main() {
 	cudaError_t cudaStatus;
 	float *gm_dev = NULL;
 	size_t x_size = 1024, y_size = 1024;
-	vector<float> f(x_size * y_size), x_diffusion(x_size*y_size), y_diffusion(x_size*y_size);
+	vector<float> f(x_size * y_size), x_diffusion(x_size * y_size), y_diffusion(x_size * y_size);
 	initial_value(f, x_size, y_size); x_initial_diffusion(x_diffusion, x_size, y_size); y_initial_diffusion(y_diffusion, x_size, y_size);
 	float rx = 1.f, ry = 1.f;
 
@@ -74,6 +76,12 @@ int main() {
 		goto Clear;
 	}
 
+	if (cudaSuccess != (cudaStatus = cudaMemcpy(gm_dev + x_size*y_size, f.data(), x_size * y_size * sizeof(float), cudaMemcpyHostToDevice))) {
+		cout << "Can't copy data from f to device:" << endl;
+		cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
+		goto Clear;
+	}
+
 	if (cudaSuccess != (cudaStatus = cudaMemcpy(gm_dev + 2*x_size*y_size, x_diffusion.data(), x_size * y_size * sizeof(float), cudaMemcpyHostToDevice))) {
 		cout << "Can't copy data from x_dfc to device:" << endl;
 		cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
@@ -90,8 +98,41 @@ int main() {
 		size_t matrix_size = x_size * y_size;
 		float *f_prev = gm_dev + y_size + 1, *f_curr = f_prev + matrix_size, *x_dfc = f_curr + matrix_size, *y_dfc = x_dfc + matrix_size, *a = y_dfc + matrix_size, *b = a + matrix_size, *c = b + matrix_size, *d = c + matrix_size;
 
-		int blockDim = 1, threads = 1024;
+		int blockDim = 1, threads = 1022;
+
+		auto begin = chrono::steady_clock::now(), end = begin;
 		diffusion::device::forward_step_multisolver_kernel<<<blockDim,threads>>>(f_prev, x_dfc, y_dfc, a, b, c, d, rx, ry, x_size - 2, y_size /*x_sitride*/, x_size /*y_stride*/);
+		if (cudaSuccess != (cudaStatus = cudaGetLastError())) {
+			cout << "Kernel launch failed!" << endl;
+			cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
+			cudaDeviceSynchronize();
+			goto Clear;
+		}
+
+		math::device::thomson_multisolver_kernell<<<blockDim,threads>>>(a,b,c,d,f_prev,x_size-2,y_size);
+		if (cudaSuccess != (cudaStatus = cudaGetLastError())) {
+			cout << "Kernel launch failed!" << endl;
+			cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
+			cudaDeviceSynchronize();
+			goto Clear;
+		}
+
+		cudaDeviceSynchronize();
+		end = chrono::steady_clock::now();
+		cerr << "Time consumed: " << chrono::duration <double, milli>(end - begin).count() << " ms" << endl;
+
+		if (cudaSuccess != (cudaStatus = cudaMemcpy(f.data(), gm_dev, x_size * y_size * sizeof(float), cudaMemcpyDeviceToHost))) {
+			cout << "Can't copy data from f_prev to host:" << endl;
+			cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
+			goto Clear;
+		}
+		else {
+			ofstream ascii_out("./data/matrix.txt");
+			ascii_out.precision(7); ascii_out.setf(ios::fixed, ios::floatfield);
+			for (size_t y_idx = 0; y_idx != y_size; ++y_idx)
+				for (size_t x_idx = 0; x_idx != x_size; ++x_idx)
+					ascii_out << x_idx << " " << y_idx << " " << f[x_idx * y_size + y_idx] << endl;
+		}
 	}
 
 Clear:
