61e81b69291581ab6d0196bf4ec39d115c2d6db2
Lionishy <lionishy@gmail.com>
2019-10-21 15:28:21 +0300

    Tridiagonal matrix testing

--- TwoDimensionalDiffusionTest.cu
+++ TwoDimensionalDiffusionTest.cu
@@ -11,11 +11,11 @@
 #include <chrono>
 
 template <typename T>
-void initial_value(std::vector<T> &matrix, size_t x_size, size_t y_size) {
+void initial_value(std::vector<T> &f, size_t x_size, size_t y_size) {
 	T grad = T(1) / (x_size-1);
 	for (size_t y_idx = 0; y_idx != y_size; ++y_idx)
 		for (size_t x_idx = 0; x_idx != x_size; ++x_idx) {
-			matrix[x_idx*y_size + y_idx] = T(1) - grad * x_idx;
+			f[x_idx*y_size + y_idx] = T(1) - grad * x_idx;
 		}
 }
 
@@ -53,6 +53,7 @@ int main() {
 	size_t x_size = 1024, y_size = 1024;
 	vector<float> f(x_size * y_size), x_diffusion(x_size * y_size), y_diffusion(x_size * y_size);
 	initial_value(f, x_size, y_size); x_initial_diffusion(x_diffusion, x_size, y_size); y_initial_diffusion(y_diffusion, x_size, y_size);
+	vector<float> host_a((x_size - 2) * (y_size - 2));
 	float rx = 1.f, ry = 1.f;
 
 	if (cudaSuccess != (cudaStatus = cudaSetDevice(0))) {
@@ -99,29 +100,31 @@ int main() {
 		float *f_prev = gm_dev + y_size + 1, *f_curr = f_prev + matrix_size, *x_dfc = f_curr + matrix_size, *y_dfc = x_dfc + matrix_size, *a = y_dfc + matrix_size, *b = a + matrix_size, *c = b + matrix_size, *d = c + matrix_size;
 
 		int blockDim = 1, threads = 1022;
-
 		auto begin = chrono::steady_clock::now(), end = begin;
-		diffusion::device::forward_step_multisolver_kernel<<<blockDim,threads>>>(f_prev, x_dfc, y_dfc, a, b, c, d, rx, ry, x_size - 2, y_size /*x_sitride*/, x_size /*y_stride*/);
-		if (cudaSuccess != (cudaStatus = cudaGetLastError())) {
-			cout << "Kernel launch failed!" << endl;
-			cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
-			cudaDeviceSynchronize();
-			goto Clear;
-		}
+		for (int count = 0; count != 1; ++count) {
+			diffusion::device::forward_step_multisolver_kernel<<<blockDim, threads>>>(f_prev, x_dfc, y_dfc, a, b, c, d, rx, ry, x_size - 2, y_size /*x_sitride*/, x_size /*y_stride*/);
+			if (cudaSuccess != (cudaStatus = cudaGetLastError())) {
+				cout << "Kernel launch failed!" << endl;
+				cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
+				cudaDeviceSynchronize();
+				goto Clear;
+			}
 
-		math::device::thomson_multisolver_kernell<<<blockDim,threads>>>(a,b,c,d,f_prev,x_size-2,y_size);
-		if (cudaSuccess != (cudaStatus = cudaGetLastError())) {
-			cout << "Kernel launch failed!" << endl;
-			cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
+			math::device::thomson_multisolver_kernell<<<blockDim, threads>>>(a, b, c, d, f_prev, x_size - 2, y_size);
+			if (cudaSuccess != (cudaStatus = cudaGetLastError())) {
+				cout << "Kernel launch failed!" << endl;
+				cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
+				cudaDeviceSynchronize();
+				goto Clear;
+			}
 			cudaDeviceSynchronize();
-			goto Clear;
 		}
 
 		cudaDeviceSynchronize();
 		end = chrono::steady_clock::now();
 		cerr << "Time consumed: " << chrono::duration <double, milli>(end - begin).count() << " ms" << endl;
 
-		if (cudaSuccess != (cudaStatus = cudaMemcpy(f.data(), gm_dev, x_size * y_size * sizeof(float), cudaMemcpyDeviceToHost))) {
+		if (cudaSuccess != (cudaStatus = cudaMemcpy(f.data(), d - (y_size + 1), x_size*y_size * sizeof(float), cudaMemcpyDeviceToHost))) {
 			cout << "Can't copy data from f_prev to host:" << endl;
 			cout << cudaStatus << " -- " << cudaGetErrorString(cudaStatus) << endl;
 			goto Clear;
@@ -129,9 +132,9 @@ int main() {
 		else {
 			ofstream ascii_out("./data/matrix.txt");
 			ascii_out.precision(7); ascii_out.setf(ios::fixed, ios::floatfield);
-			for (size_t y_idx = 0; y_idx != y_size; ++y_idx)
-				for (size_t x_idx = 0; x_idx != x_size; ++x_idx)
-					ascii_out << x_idx << " " << y_idx << " " << f[x_idx * y_size + y_idx] << endl;
+			for (size_t y_idx = 0; y_idx != y_size-2; ++y_idx)
+				for (size_t x_idx = 0; x_idx != x_size-2; ++x_idx)
+					ascii_out << x_idx << " " << y_idx << " " << f[y_size + 1 + x_idx * y_size + y_idx] << endl;
 		}
 	}
 
