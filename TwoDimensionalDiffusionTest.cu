fd3eb3677d5addc6749dc49b5482368950e89a91
Mark Shevelev <lionishy@gmail.com>
2019-10-21 10:22:19 +0300

    Main file started

---
+++ TwoDimensionalDiffusionTest.cu
@@ -0,0 +1,18 @@
+
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+int main() {
+
+	/**
+	 * Loop over...
+	 * ForwardCalculationKernell -> ThomsonKernell -> TransposeKernell -> CorrectionCalculationKernell -> ThomsonKernell -> TransposeKernell
+	 */
+
+
+	return 0;
+}
