10a909a7d3a08f355d4b32043715f1dc131cf479
Mark Shevelev <lionishy@gmail.com>
2019-10-30 20:01:12 +0300

    Diagonal discretization -- operations oreder changed

--- DiagonalDiscretization.cuh
+++ DiagonalDiscretization.cuh
@@ -7,12 +7,12 @@
 namespace iki {	namespace diffusion { namespace device {
 	template <typename T> inline
 		T __device__ diagonal_discretization(T fim1, T fi, T fip1, T dfc_left, T dfc_right) {
-		return dfc_left * fim1 + dfc_right * fip1 - (dfc_left + dfc_right) * fi;
+		return dfc_left * (fim1 - fi) + dfc_right * (fip1 - fi);
 	}
 
 	template <> inline
 		float __device__ diagonal_discretization<float>(float fim1, float fi, float fip1, float dfc_left, float dfc_right) {
-		return __fmaf_rn(dfc_left, fim1, __fmaf_rn(dfc_right, fip1, -__fmaf_rn(dfc_left, fi, dfc_right * fi)));
+		return __fmaf_rn(dfc_left, fim1, -dfc_left * fi) + __fmaf_rn(dfc_right, fip1, -dfc_right * fi);
 	}
 } /* device */ } /* math */ } /* iki */
 
