1badcfd8ee0ca54335d85290e69e3e52027c19f9
Mark Shevelev <lionishy@gmail.com>
2019-11-04 10:35:24 +0300

    Mixed term x_y_sin_sin stability test added

--- DiffusionTest.h
+++ DiffusionTest.h
@@ -171,6 +171,40 @@ namespace iki {	namespace diffusion {
 					yx_dfc[y_idx + x_idx * y_size] = T(1);
 		}
 	}
+
+	template <typename T>
+	void x_y_sin_sin_mixed_term_test(std::vector<T> &f, std::vector<T> &x_dfc, std::vector<T> &y_dfc, std::vector<T> &xy_dfc, std::vector<T> &yx_dfc, size_t x_size, size_t y_size, int Nx, int Ny) {
+		{
+			auto const PI = T(3.14159265358979323);
+			for (size_t y_idx = 0; y_idx != y_size; ++y_idx)
+				for (size_t x_idx = 0; x_idx != x_size; ++x_idx)
+					f[x_idx * y_size + y_idx] = std::sin((PI * Nx) / (x_size - 1) * x_idx) * std::sin((PI * Ny) / (y_size - 1) * y_idx);
+		}
+
+		{
+			for (size_t y_idx = 0; y_idx != y_size; ++y_idx)
+				for (size_t x_idx = 0; x_idx != x_size; ++x_idx)
+					x_dfc[y_idx + x_idx * y_size] = T(1);
+		}
+
+		{
+			for (size_t y_idx = 0; y_idx != y_size; ++y_idx)
+				for (size_t x_idx = 0; x_idx != x_size; ++x_idx)
+					y_dfc[y_idx + x_idx * y_size] = T(1);
+		}
+
+		{
+			for (size_t y_idx = 2; y_idx != y_size - 3; ++y_idx)
+				for (size_t x_idx = 2; x_idx != x_size - 3; ++x_idx)
+					xy_dfc[y_idx + x_idx * y_size] = T(1);
+		}
+
+		{
+			for (size_t y_idx = 2; y_idx != y_size - 3; ++y_idx)
+				for (size_t x_idx = 2; x_idx != x_size - 3; ++x_idx)
+					yx_dfc[y_idx + x_idx * y_size] = T(1);
+		}
+	}
 } /* diffusion */ } /* iki */
 
 #endif /* DiffusionTest_H */
