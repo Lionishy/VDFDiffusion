2dafd5540829df79c431cf80b5bd8b5ce8e1053a
Lionishy <lionishy@gmail.com>
2019-11-08 13:57:29 +0300

    Initial data import (function & diffusion coefficients pivots)

--- InitialDataImport.h
+++ InitialDataImport.h
@@ -2,4 +2,24 @@
 #ifndef InitialDataImport_H
 #define InitialDataImport_H
 
+#include "SimpleTable.h"
+#include "SimpleTableIO.h"
+
+#include <iostream>
+#include <vector>
+
+namespace iki { namespace diffusion { 
+	template <typename T>
+	std::istream& import_initial_data(std::istream &binary_is, UniformSimpleTable<T, 2u, 5u> &f_dfc_table, std::vector<T> &f_dfc_data) {
+		read_binary(binary_is, f_dfc_table.space);
+		read_binary(binary_is, f_dfc_table.bounds);
+
+		f_dfc_data.resize(collapsed_size<Dim>(&f_dfc_table.bounds));
+		f_dfc_table.data = f_dfc_data.data();
+		read_binary(binary_is, f_dfc_table);
+
+		return binary_is;
+	}
+} /* diffusion */ } /* iki */
+
 #endif /* InitialDataImport_H */
