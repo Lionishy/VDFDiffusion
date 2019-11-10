031d3594a90bae4ec488f968b238ac570324c2ad
Mark Shevelev <lionishy@gmail.com>
2019-11-10 14:55:12 +0300

    First moment with Kahan summation added -- device based

---
+++ FirstMoment.cuh
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef FirstMoment_CUH
+#define FirstMoment_CUH
+
+#include <cuda_runtime.h>
+
+namespace iki {	namespace math { namespace device {
+	template <typename T>
+	__device__ void first_moment(T const *f, T start, T dx, unsigned size, unsigned stride, T *s, T *r) {
+		T sum = T(0), rem = T(0), y, t;
+		for (unsigned fidx = 0, fend = size * stride, arg_idx = 0; idx != end; idx += stride, ++arg_idx) {
+			y = (start + dx * arg_idx) * f[fidx] - rem;
+			t = sum + y;
+			rem = (t - sum) - y;
+			sum = t;
+		}
+		*s = sum;
+		*r = rem;
+	}
+} /* device */ } /* math */ } /* iki */
+
+
+#endif /* FirstMoment_CUH */
