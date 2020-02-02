7fe2444b2a4061274b2fce273b59cbc36e1b5667
Mark Shevelev <lionishy@gmail.com>
2020-02-02 20:51:32 +0300

    Amplitude kernel added

---
+++ AmplitudeKernel.cuh
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef AmplitudeKernle_CUH
+#define AmplitudeKernle_CUH
+
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+namespace iki {	namespace whfi { namespace device {
+	template <typename T>
+	__global__ void amplitude_update_kernell(T const *gamma, T const *amplitude_spectrum, T dt, unsigned size) {
+		unsigned shift = blockIdx.x * blockDim.x + threadIdx.x;
+		if (0 == shift || size - 1 == shift) amplitude_spectrum[shift] = T(0.);
+		amplitude_spectrum[shift] += T(2.0) * dt * gamma[shift] * amplitude_spectrum[shift];
+	}
+} /*device*/ } /*whfi*/ } /*iki*/
+
+#endif /*AmplitudeKernle_CUH*/
