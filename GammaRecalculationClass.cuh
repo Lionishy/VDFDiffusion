4713caadd8d3ba35eeb3a47d2e309e46902099fb
Mark Shevelev <lionishy@gmail.com>
2020-02-03 11:12:04 +0300

    GammapRecalculation class constructor added

--- GammaRecalculationClass.cuh
+++ GammaRecalculationClass.cuh
@@ -3,29 +3,55 @@
 #include "ZeroMoment.cuh"
 #include "FirstMoment.cuh"
 #include "AmplitudeKernel.cuh"
+#include "DeviceException.h"
+
+#include <vector>
 
 namespace iki { namespace whfi {
 	template <typename T>
 	struct GammaReclaculation {
 
+		GammaReclaculation(size_t vperp_size, size_t vparall_size, UniformSpace<T, 2u> velocity_space, T dt, std::vector<T> const &x_dfc_pivot_host, std::vector<T> const &y_dfc_pivot_host, std::vector<T> const &xy_dfc_pivot_host, std::vector<T> const &yx_dfc_pivot_host, std::vector<T> const &dispersion_derivative_host, std::vector<T> const &k_betta_host): 
+			vperp_size(vperp_size), vparall_size(vparall_size), 
+			velocity_space(velocity_space), 
+			dt(dt), 
+			vdf(nullptr), x_dfc(nullptr), y_dfc(nullptr), xy_dfc(nullptr), yx_dfc(nullptr), 
+			internal_device_memory(4*vperp_size*vparall_size*sizeof(T) + 6*vparall_size*sizeof(T)) {
+
+			//initial data copy
+			{
+				cudaError_t cudaStatus;
+				if (cudaSuccess != (cudaStatus = cudaMemcpy(x_dfc_pivot, x_dfc_pivot_host.data(), vperp_size * vparall_size * sizeof(T), cudaMemcpyHostToDevice))) throw DeviceException(cudaStatus);
+				if (cudaSuccess != (cudaStatus = cudaMemcpy(y_dfc_pivot, y_dfc_pivot_host.data(), vperp_size * vparall_size * sizeof(T), cudaMemcpyHostToDevice))) throw DeviceException(cudaStatus);
+				if (cudaSuccess != (cudaStatus = cudaMemcpy(xy_dfc_pivot, xy_dfc_pivot_host.data(), vperp_size * vparall_size * sizeof(T), cudaMemcpyHostToDevice))) throw DeviceException(cudaStatus);
+				if (cudaSuccess != (cudaStatus = cudaMemcpy(yx_dfc_pivot, yx_dfc_pivot_host.data(), vperp_size * vparall_size * sizeof(T), cudaMemcpyHostToDevice))) throw DeviceException(cudaStatus);
+
+				if (cudaSuccess != (cudaStatus = cudaMemcpy(dispersion_derivative, dispersion_derivative_host.data(), vparall_size * sizeof(T), cudaMemcpyHostToDevice))) throw DeviceException(cudaStatus);
+				if (cudaSuccess != (cudaStatus = cudaMemcpy(k_betta, k_betta_host.data(), vparall_size * sizeof(T), cudaMemcpyHostToDevice))) throw DeviceException(cudaStatus);
+			}
+		}
+
+		size_t vperp_size, vparall_size; //vperp - x; vparall - y
+		UniformSpace<T, 2u> velocity_space; //vperp - 0, vparall - 1
+		T dt;
+
 		T const *vdf;                       //external memory to be read
 		T *x_dfc, *y_dfc, *xy_dfc, *yx_dfc; //external memory to be written
 
 		DeviceMemory internal_device_memory;
-		T *dispersion_derivative, *k_betta;                         //readonly internal memory
-		T *x_dfc_pivot, *y_dfc_pivot, *xy_dfc_pivot, *yx_dfc_pivot; //readonly internal memory
+		T *x_dfc_pivot, *y_dfc_pivot, *xy_dfc_pivot, *yx_dfc_pivot; //predefined data
+		T *dispersion_derivative, *k_betta;                         //predefined data
 		T *growth_rate_spectrum, *amplitude_spectrum, *zero_moment, *first_moment; //internal memory to be updated at every step
-
-		Bounds<2u> size;
-		UniformSpace<T, 2u> velocity_space;
+		
+		
 
 		void growth_rate_update() {
-			math::device::zero_moment_kernel << <1, size.components[1] >> > (vdf, velocity_space.axes[0].begin, velocity_space.axes[0].step, size.components[0], size.components[1], zero_moment);
-			math::device:first_moment_kernel <<<1, size.components[1]>>> (vdf, velocity_space.axes[0].begin, velocity_space.axes[0].step, size.components[0], size.components[1], first_moment);
+			math::device::zero_moment_kernel <<<1,vparall_size>>> (vdf, velocity_space.axes[0].begin, velocity_space.axes[0].step, vperp_size, vparall_size, zero_moment);
+			math::device:first_moment_kernel <<<1,vparall_size>>> (vdf, velocity_space.axes[0].begin, velocity_space.axes[0].step, vperp_size, vparall_size, first_moment);
 
-			device::gamma_kernel << 1, size.components[1] >> > (zero_moment, first_index, k_betta, dispersion_derivative, velocity_space.axes[1].step, size.components[1], growth_rate_spectrum);
+			device::gamma_kernel <<<1,vparall_size>>> (zero_moment, first_moment, k_betta, dispersion_derivative, velocity_space.axes[1].step, vparall_size, growth_rate_spectrum);
 
-			device::amplitude_update_kernell <<<1, size.components[1] >>> (growth_rate_spectrum, amplitude_spectrum, dt, size.components[1]);
+			device::amplitude_update_kernell <<<1,vparall_size>>> (growth_rate_spectrum, amplitude_spectrum, dt, vparall_size);
 		}
 	};
 
