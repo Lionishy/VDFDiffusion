aea52668f355c57f85fb65a316eda4b66beda00e
Lionishy <lionishy@gmail.com>
2020-02-05 15:41:05 +0300

    DispersionRelationOmegaDerive input parameter changed to k betta core product

--- DispersionRelationOmegaDerive.h
+++ DispersionRelationOmegaDerive.h
@@ -8,12 +8,13 @@
 namespace iki { namespace whfi { 
 	template <typename T>
 	struct DispersionRelationOmegaDerivative {
-		T operator()(T omega, T k) {
-			T arg_c = (omega - T(1.)) / (k * params.betta_root_c) - params.bulk_to_term_c;
-			T arg_h = (omega - T(1.)) / (k * params.betta_root_h) - params.bulk_to_term_h;
+		T operator()(T omega, T k_betta_c) {
+			T k_betta_h = k_betta_c / params.TcTh_ratio_root;
+			T arg_c = (omega - T(1.)) / (k_betta_c) - params.bulk_to_term_c;
+			T arg_h = (omega - T(1.)) / (k_betta_h) - params.bulk_to_term_h;
 			T Zc = Z(arg_c), Zh = Z(arg_h);
-			return params.nc / (k * params.betta_root_c) * (-Zc + (omega / (k * params.betta_root_c) - params.bulk_to_term_c) * (Zc * arg_c + T(1.)))
-				+ params.nh / (k * params.betta_root_h) * (-Zh + (omega / (k * params.betta_root_h) - params.bulk_to_term_h) * (Zh * arg_h + T(1.)));
+			return params.nc / (k_betta_c) * (-Zc + (omega / (k_betta_c) - params.bulk_to_term_c) * (Zc * arg_c + T(1.)))
+				+ params.nh / (k_betta_h) * (-Zh + (omega / (k_betta_h) - params.bulk_to_term_h) * (Zh * arg_h + T(1.)));
 		}
 
 		DispersionRelationOmegaDerivative(ZFunc Z, PhysicalParameters<T> params) : Z(Z), params(params) { }
