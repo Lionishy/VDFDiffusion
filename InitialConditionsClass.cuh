8eb9fcaa4f35f5a9eae601241bff8c00efc1a2cd
Lionishy <lionishy@gmail.com>
2020-02-05 14:00:01 +0300

    omega calculation added

--- InitialConditionsClass.cuh
+++ InitialConditionsClass.cuh
@@ -5,8 +5,12 @@
 #include "ZFunc.h"
 #include "SimpleTable.h"
 #include "PhysicalParameters.h"
+#include "StepSolver.h"
+#include "ResonantVelocityEqn.h"
 
 #include <vector>
+#include <iterator>
+#include <algorithm>
 
 namespace iki { namespace whfi {
 	template <typename T>
@@ -17,6 +21,14 @@ namespace iki { namespace whfi {
 	template <typename T>
 	struct InitialConditionsCalculation {
 		InitialConditions<T> recalculate(PhysicalParameters<T> params, T v_parall_begin, T v_parall_step, size_t v_parall_size) const {
+			InitialConditions<T> result;
+			for (size_t count = 0; count != v_parall_size; ++count) {
+				result.omega.push_back(
+					math::step_solver(ResonantVelocityEqn<T>(v_parall_begin + count * v_parall_step, params, Z), T(1.e-5), T(1.e-5), T(1. + 1.e-6))
+				);
+			}
+
+
 
 		}
 
