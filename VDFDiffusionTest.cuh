7fe38783d7e469b586782a4deac57c6c29dcf5d4
Lionishy <lionishy@gmail.com>
2020-02-21 16:53:05 +0300

    VDF represented as a table

--- VDFDiffusionTest.cuh
+++ VDFDiffusionTest.cuh
@@ -3,25 +3,32 @@
 
 #include "Pow.h"
 
+#include <vector>
+
 int main() {
 	using namespace std;
 	using namespace iki;
 	using namespace iki::diffusion;
 
-	size_t vparall_size = 512, vperp_size = 512;
-	UniformSpace<float, 2u> v_space;
-	v_space.axes[0].begin = -15.0f; //vparall
-	v_space.axes[0].step = 1.3e-2f;
-	v_space.axes[1].begin = 0.f;    //vperp
-	v_space.axes[1].step = 5e-2f;
+	UniformSimpleTable<float, 2u, 1u> vdf_table;
+	vector<float> vdf_table_data;
+	{ //initialize vdf_table
+		vdf_table.bounds.components[0] = vdf_table.bounds.components[1] = 512;
+		vdf_table.space.axes[0].begin = -15.0f; //vparall
+		vdf_table.space.axes[0].step = 1.3e-2f;
+		vdf_table.space.axes[1].begin = 0.f;    //vperp
+		vdf_table.space.axes[1].step = 5e-2f;
+		vdf_table_data.resize(collapsed_size(&vdf_table.bounds));
+		vdf_table.data = vdf_table_data.data();
+	}
 
 	float dt = 1.f; //dt = 1./omega_c
-	float rparall = dt / math::pow<2u>(v_space.axes[0].step), rperp = dt / math::pow<2u>(v_space.axes[1].step);
+	float rparall = dt / math::pow<2u>(vdf_table.space.axes[0].step), rperp = dt / math::pow<2u>(vdf_table.space.axes[1].step);
 
 
 
 
-	SimpleTwoDimensionalSolver<float> diffusion_solver(vparall_size, vperp_size, rparall, rperp, );
+	SimpleTwoDimensionalSolver<float> vdf_diffusor(vdf_table.bounds.components[0], vdf_table.bounds.components[1], rparall, rperp, );
 
 	return 0;
 }
